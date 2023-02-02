f9791f5ea9646b6df2200ac86ff7ec2fe4fb9a7c
augusto <augustosaaude@gmail.com>
2023-02-01 22:08:13 -0300

    feat: adicionei os click dos menus no app

--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.motivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.motivation.infra.MotivationConstants
 import com.example.motivation.R
 import com.example.motivation.infra.SecurityPreferences
@@ -10,6 +11,7 @@ import com.example.motivation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
+    private var categoryId = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,14 +20,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         supportActionBar?.hide()
 
+        handleFilter(R.id.image_all_inclusive)
+
         handleUserName()
 
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAllInclusive.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
             println("oi")
+        } else if (view.id in listOf(R.id.image_all_inclusive, R.id.image_happy, R.id.image_sunny)) {
+            handleFilter(view.id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+        binding.imageAllInclusive.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when(id) {
+            R.id.image_all_inclusive -> {
+                binding.imageAllInclusive.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
         }
     }
 
